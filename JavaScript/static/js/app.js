11f59458fe2b466106271a4946907ac2d00bfd36
Pollock Matisse <paul.h.westfall@gmail.com>
2019-03-11 20:28:05 -0400

    js hw2

--- JavaScript/static/js/app.js
+++ JavaScript/static/js/app.js
@@ -56,34 +56,9 @@ filterButton.on("click", function() {
       }
 
 });
-//     var inputDate = inputField1.property("value").trim();
-//     var inputCity = inputField2.property("value").toLowerCase().trim();
-//   // Filter by field matching input value
-//     var filterDate = tableData.filter(data => data.datetime === inputDate);
-//         console.log(filterDate)
-//     var filterCity = tableData.filter(data => data.city === inputCity);
-//         console.log(filterCity)
-//     var filterData = tableData.filter(data => data.datetime === inputDate && data.city === inputCity);
-//         console.log(filterData)
-
-// //   Add filtered sighting to table
-//   tbody.html("");
-
-//   let response = {
-//     filterData, filterCity, filterDate
-//   }
-
-//   if (response.filterData.length !== 0) {
-//     populate(filterData);
-//   }
-//     else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-//       populate(filterCity) || populate(filterDate);
-  
-//     }
-//     else {
-//       tbody.append("tr").append("td").text("No results found!"); 
-//     }
-// });
+//     
+//     
+// 
 
 resetbtn.on("click", () => {
   tbody.html("");
