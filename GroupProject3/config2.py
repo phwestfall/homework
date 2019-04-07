80f83230f2b21637e3494dea94b77a5daa2f6568
Pollock Matisse <paul.h.westfall@gmail.com>
2019-04-07 12:50:27 -0400

    here we go

---
+++ GroupProject3/config2.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = "morbid.cu97gshsimlg.us-east-1.rds.amazonaws.com"
+remote_db_port = 3306
+remote_morbid_dbname = "morbid"
+remote_morbid_dbuser = "root"
+remote_morbid_dbpwd = "Morbid123"
